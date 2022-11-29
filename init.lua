ed2db905b9d4fea910b29eb4ac2e681494957697
Fabian Barajas <fbarajas@nvidia.com>
2022-11-28 21:17:27 -0800

    heirline mod to show filepath for current buffer

--- init.lua
+++ init.lua
@@ -268,6 +268,26 @@ local config = {
                         -- },
                 },
                 -- All other entries override the require("<key>").setup({...}) call for default plugins
+                heirline = function(config)
+                        config[1] = {
+                                hl = { fg = "fg", bg = "bg" },
+                                astronvim.status.component.mode(),
+                                astronvim.status.component.git_branch(),
+                                astronvim.status.component.file_info {
+                                        filename = { modify = ":." },
+                                },
+                                astronvim.status.component.git_diff(),
+                                astronvim.status.component.diagnostics(),
+                                astronvim.status.component.fill(),
+                                astronvim.status.component.macro_recording(),
+                                astronvim.status.component.fill(),
+                                astronvim.status.component.lsp(),
+                                astronvim.status.component.treesitter(),
+                                astronvim.status.component.nav(),
+                                astronvim.status.component.mode { surround = { separator = "right" } },
+                        }
+                        return config
+                end,
                 ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
                         -- config variable is the default configuration table for the setup functino call
                         -- local null_ls = require "null-ls"
