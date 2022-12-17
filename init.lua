aca857e206f8358fc8ce731a04acd43890642e1a
Fabian Barajas <fbarajas@nvidia.com>
2022-12-16 19:31:58 -0800

    adding chatgpt.nvim plugin

--- init.lua
+++ init.lua
@@ -233,6 +233,19 @@ local config = {
                                         }
                                 end
                         },
+                        {
+                                "jackMort/ChatGPT.nvim",
+                                  config = function()
+                                    require("chatgpt").setup({
+                                      -- optional configuration
+                                    })
+                                  end,
+                                  requires = {
+                                    "MunifTanjim/nui.nvim",
+                                    "nvim-lua/plenary.nvim",
+                                    "nvim-telescope/telescope.nvim"
+                                  }
+                        },
                         {"ellisonleao/glow.nvim"},
                         { "tpope/vim-surround" },
                         -- TODO: set up debugging DAP
