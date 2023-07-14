e06051c18811acd3e93f9ec001e5d6149a6aa199
Fabian Barajas <fbarajas@nvidia.com>
2023-07-14 11:23:02 -0700

    format_on_save enabled by default but disabled for yaml files

--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
 return {
     colorscheme = "monokai",
 
+    lsp = {
+        formatting = {
+            format_on_save = {
+                enabled = true,
+                ignore_filetypes = {
+                    "yaml",
+                }
+            },
+        },
+    },
     -- This function is run last and is a good place to configuring
     -- augroups/autocommands and custom filetypes also this just pure lua so
     -- anything that doesn't fit in the normal config locations above can go here
@@ -20,6 +30,31 @@ return {
         if not status_ok then
             return
         end
+
+        -- enable anisble-language-server for ansible files
+        -- TODO: Need to determine why this isn't loading. May be due to docker_compose lsp attaching to yaml filetype
+        local function yaml_ft(path, bufnr)
+            -- get content of buffer as string
+            local content = vim.filetype.getlines(bufnr)
+            if type(content) == "table" then content = table.concat(content, "\n") end
+
+            -- check if file is in roles, tasks, or handlers folder
+            local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
+            if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
+            -- check for known ansible playbook text and if found, return yaml.ansible
+            local regex = vim.regex "hosts:\\|tasks:"
+            if regex and regex:match_str(content) then return "yaml.ansible" end
+
+            -- return yaml if nothing else
+            return "yaml"
+        end
+
+        vim.filetype.add {
+            extension = {
+                yml = yaml_ft,
+                yaml = yaml_ft,
+            },
+        }
         -- set custom fuctions
         -- Set up custom filetypes
         -- vim.filetype.add {
