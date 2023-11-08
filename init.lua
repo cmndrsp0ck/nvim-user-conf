c69b4262b462ccfcac30254d6d4a6cca042778e2
Fabian Barajas <fbarajas@nvidia.com>
2023-11-08 00:38:35 -0800

    setting file name inventory to filetype ini

--- init.lua
+++ init.lua
@@ -55,48 +55,52 @@ return {
                 yaml = yaml_ft,
             },
         }
+        vim.filetype.add {
+            filename = {
+                ["inventory"] = "ini",
+            },
+        }
         -- set custom fuctions
         -- Set up custom filetypes
         -- vim.filetype.add {
-            --   extension = {
-                --     foo = "fooscript",
-                --   },
-                --   filename = {
-                    --     ["Foofile"] = "fooscript",
-                    --   },
-                    --   pattern = {
-                        --     ["~/%.config/foo/.*"] = "fooscript",
-                        --   },
-                        -- },
+        --   extension = {
+        --     foo = "fooscript",
+        --   },
+        --   filename = {
+        --     ["Foofile"] = "fooscript",
+        --   },
+        --   pattern = {
+        --     ["~/%.config/foo/.*"] = "fooscript",
+        --   },
+        -- },
     end,
 }
 
 -- TODO: need to make adjustments to status bar (filename.ext and full mode text)
 -- return {
-    --   plugins = {
-        --     {
-            --       "rebelot/heirline.nvim",
-            --       opts = function(_, opts)
-                --         local status = require("astronvim.utils.status")
-                --         opts.statusline = { -- statusline
-                --           hl = { fg = "fg", bg = "bg" },
-                --           status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-                --           status.component.git_branch(),
-                --           status.component.file_info { filetype = {}, filename = false, file_modified = false },
-                --           status.component.git_diff(),
-                --           status.component.diagnostics(),
-                --           status.component.fill(),
-                --           status.component.cmd_info(),
-                --           status.component.fill(),
-                --           status.component.lsp(),
-                --           status.component.treesitter(),
-                --           status.component.nav(),
-                --           -- remove the 2nd mode indicator on the right
-                --         }
-                --         -- return the final configuration table
-                --         return opts
-                --       end
-                --     },
-                --   },
-                -- }
-
+--   plugins = {
+--     {
+--       "rebelot/heirline.nvim",
+--       opts = function(_, opts)
+--         local status = require("astronvim.utils.status")
+--         opts.statusline = { -- statusline
+--           hl = { fg = "fg", bg = "bg" },
+--           status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+--           status.component.git_branch(),
+--           status.component.file_info { filetype = {}, filename = false, file_modified = false },
+--           status.component.git_diff(),
+--           status.component.diagnostics(),
+--           status.component.fill(),
+--           status.component.cmd_info(),
+--           status.component.fill(),
+--           status.component.lsp(),
+--           status.component.treesitter(),
+--           status.component.nav(),
+--           -- remove the 2nd mode indicator on the right
+--         }
+--         -- return the final configuration table
+--         return opts
+--       end
+--     },
+--   },
+-- }
